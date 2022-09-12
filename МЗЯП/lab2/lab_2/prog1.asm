bf3c5489b261e42160e8f0ded1c7d11e17da2c7f
Slava <Anohin.VA@yandex.ru>
2022-09-12 16:50:39 +0300

    masm

---
+++ МЗЯП/lab2/lab_2/prog1.asm
@@ -0,0 +1,23 @@
+.686P
+.MODEL FLAT, STDCALL
+.STACK 4096
+.DATA
+myByte   BYTE  13
+myByte2  BYTE  ?
+myWord   Word  25
+myWord2  Word  ?
+myDWord  dword 123
+myDWord2 dword ?
+myString byte  "0123456789", 0
+myFloat  real4 3.14
+myFloat2 real4 ?
+.CODE
+START:
+mov al, myByte
+mov myByte2, al
+mov ax, myWord
+mov myWord2, ax
+mov eax, myDWord
+mov myDWord2, eax
+RET
+END START
