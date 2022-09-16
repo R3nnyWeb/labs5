8e83f4fc76f30d59a48c501a3c5a476bbd4b69dc
Slava <Anohin.VA@yandex.ru>
2022-09-16 15:24:14 +0300

    javaGraphic

---
+++ Компьютерная графика/ex1/src/FirstPack/Window.java
@@ -0,0 +1,13 @@
+package FirstPack;
+
+import javax.swing.*;
+
+public class Window {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("EVM forever");
+        jFrame.setSize(200, 200);
+        jFrame.setResizable(true);
+        jFrame.setVisible(true);
+    }
+}
