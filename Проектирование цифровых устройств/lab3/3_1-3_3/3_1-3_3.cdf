e58c95aeb43687f48f6d57fbef06df9cf008476f
R3nnyWeb <nikitavaskulatov@gmail.com>
2022-09-21 14:29:19 +0300

    31-33

---
+++ Проектирование цифровых устройств/lab3/3_1-3_3/3_1-3_3.cdf
@@ -0,0 +1,13 @@
+/* Quartus II Version 8.1 Build 163 10/28/2008 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EPM240T100) Path("E:/3_1-3_3/") File("3_1-3_3.pof") MfrSpec(OpMask(1) SEC_Device(EPM240T100) Child_OpMask(2 1 1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ Проектирование цифровых устройств/lab3/3_1-3_3/v_33_indicator.v
@@ -0,0 +1,12 @@
+ module v33_indicator (k4,k3,k2,k1, a,b,c,d,e,f,g,h,z);
+input k4,k3,k2,k1;  output a,b,c,d,e,f,g, h,z; 
+assign a=  k4&k3&k2&~k1| k4&~k3&k2&k1;
+assign b= k4&~k3&k2&~k1|k4&~k3&~k2&k1;
+assign c=  k4&k3&~k2&k1;
+assign d= k4&k3&k2&~k1|k4&~k3&k2&k1 | k4&~k3&~k2&~k1 ;
+assign e= k4&k3&k2&~k1|k4&k3&~k2&~k1 | k4&~k3&k2&k1 | k4&~k3&k2&~k1 
+    | k4&~k3&~k2&~k1 | ~k4&k3&k2&~k1;
+assign f=  k4&k3&k2&~k1|k4&k3&~k2&k1 | k4&k3&~k2&~k1 | k4&~k3&~k2&~k1;
+assign g=  k4&k3&k2&k1|k4&k3&k2&~k1 | k4&~k3&~k2&~k1 ;
+assign h=0; assign z=0; 
+endmodule
