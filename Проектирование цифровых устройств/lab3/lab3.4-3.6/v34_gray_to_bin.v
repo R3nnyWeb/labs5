94ddcef36af31115ff23aecfde4e2013d8e28cbf
R3nnyWeb <nikitavaskulatov@gmail.com>
2022-10-20 21:43:52 +0300

    lab3_1-3_5

--- Проектирование цифровых устройств/lab3/lab3.4-3.6/v34_gray_to_bin.v
+++ Проектирование цифровых устройств/lab3/lab3.4-3.6/v34_gray_to_bin.v
@@ -1,9 +1,9 @@
-//Код Грея -  в двоичный	      //1
-module v34_gray_to_bin (g, b);	  //2
-input [3:0]g;					  //3
-output [3:0] b;					  //4
-assign b[3]= g[3];				  //5
-assign b[2]= g[3]^g[2];			  //6
-assign b[1]= g[3]^g[2]^g[1]; 	  //7
-assign b[0]= g[3]^g[2]^g[1]^g[0]; //8
-endmodule						  //9
+//Код Грея -  в двоичный	     
+module v34_gray_to_bin (g, b);	  
+input [3:0]g;					  
+output [3:0] b;					 
+assign b[3]= g[3];				  
+assign b[2]= g[3]^g[2];			  
+assign b[1]= g[3]^g[2]^g[1]; 	  
+assign b[0]= g[3]^g[2]^g[1]^g[0]; 
+endmodule						  
---
+++ Проектирование цифровых устройств/lab3/lab3.4-3.6/v34_gray_to_bin.v.bak
@@ -0,0 +1,9 @@
+//Код Грея -  в двоичный	      //1
+module v34_gray_to_bin (g, b);	  //2
+input [3:0]g;					  //3
+output [3:0] b;					  //4
+assign b[3]= g[3];				  //5
+assign b[2]= g[3]^g[2];			  //6
+assign b[1]= g[3]^g[2]^g[1]; 	  //7
+assign b[0]= g[3]^g[2]^g[1]^g[0]; //8
+endmodule						  //9
